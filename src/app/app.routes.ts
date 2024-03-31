9c281077a3f1ba7ef9a3847971230d5f9d37000a
Jordan Cooper <jordancooper1195@gmail.com>
2024-03-31 17:56:45 +1300

    Move components into components dir

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { isLoggedInGuard } from './guards/is-logged-in.guard';
 
 export const routes: Routes = [
