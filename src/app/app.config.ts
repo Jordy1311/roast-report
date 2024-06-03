822f618ee45f6dee05b584167551d0b3f92c17bd
Jordan Cooper <jordancooper1195@gmail.com>
2024-06-03 17:06:11 +1200

    Adds Angular Material (AM) v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,15 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import {
-  HttpClientModule,
-  provideHttpClient,
-  withInterceptors
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { jwtInterceptorInterceptor } from './interceptors/jwt-interceptor.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    provideHttpClient(withInterceptors([ jwtInterceptorInterceptor ])),
+    provideHttpClient(withInterceptors([ jwtInterceptorInterceptor ])), provideAnimationsAsync(),
   ]
 };
