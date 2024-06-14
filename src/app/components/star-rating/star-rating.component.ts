63dd85477c62fa5ad69daff481c7a88a4a4fe9dc
Jordan Cooper <jordancooper1195@gmail.com>
2024-06-14 14:49:46 +1200

    Makes rating component more a11y

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -1,73 +1,102 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-star-rating',
   standalone: true,
-  imports: [ MatIcon ],
+  imports: [ MatIcon, MatButtonModule ],
   styleUrl: './star-rating.component.scss',
   template: `
     <div class="container" [class.readonly]="readonly">
-      <mat-icon
-        class="material-symbols-rounded"
-        (click)="setRating(0)"
-        class="remove-rating"
-        aria-hidden="false"
-        aria-label="Choose to not specify a rating"
-      >
-        close
-      </mat-icon>
+      @if (!readonly) {
+        <button
+          mat-icon-button
+          (click)="setRating(0)"
+          [disabled]="readonly"
+          aria-label="Choose to not specify a rating"
+        >
+          <mat-icon class="material-symbols-rounded" aria-hidden="true">
+            cancel
+          </mat-icon>
+        </button>
+      }
 
-      <mat-icon
-        class="material-symbols-rounded"
+      <button
+        mat-icon-button
         (click)="setRating(1)"
-        [class.rated]="rating >= 1"
-        aria-hidden="false"
+        [disabled]="readonly"
         aria-label="One star out of five"
       >
-        star
-      </mat-icon>
+        <mat-icon
+          class="material-symbols-rounded"
+          [class.rated]="rating >= 1"
+          aria-hidden="true"
+        >
+          star
+        </mat-icon>
+      </button>
 
-      <mat-icon
-        class="material-symbols-rounded"
+      <button
+        mat-icon-button
         (click)="setRating(2)"
-        [class.rated]="rating >= 2"
-        aria-hidden="false"
+        [disabled]="readonly"
         aria-label="Two stars out of five"
       >
-        star
-      </mat-icon>
+        <mat-icon
+          class="material-symbols-rounded"
+          [class.rated]="rating >= 2"
+          aria-hidden="true"
+        >
+          star
+        </mat-icon>
+      </button>
 
-      <mat-icon
-        class="material-symbols-rounded"
+      <button
+        mat-icon-button
         (click)="setRating(3)"
-        [class.rated]="rating >= 3"
-        aria-hidden="false"
-        aria-label="Two stars out of five"
+        [disabled]="readonly"
+        aria-label="Three stars out of five"
       >
-        star
-      </mat-icon>
+        <mat-icon
+          class="material-symbols-rounded"
+          [class.rated]="rating >= 3"
+          aria-hidden="true"
+        >
+          star
+        </mat-icon>
+      </button>
 
-      <mat-icon
-        class="material-symbols-rounded"
+      <button
+        mat-icon-button
         (click)="setRating(4)"
-        [class.rated]="rating >= 4"
-        aria-hidden="false"
-        aria-label="Two stars out of five"
+        [disabled]="readonly"
+        aria-label="Four stars out of five"
       >
-        star
-      </mat-icon>
+        <mat-icon
+          class="material-symbols-rounded"
+          [class.rated]="rating >= 4"
+          aria-hidden="true"
+        >
+          star
+        </mat-icon>
+      </button>
 
-      <mat-icon
-        class="material-symbols-rounded"
+      <button
+        mat-icon-button
         (click)="setRating(5)"
-        [class.rated]="rating >= 5"
-        aria-hidden="false"
-        aria-label="Two stars out of five"
+        [disabled]="readonly"
+        aria-label="Five stars out of five"
       >
-        star
-      </mat-icon>
+        <mat-icon
+          class="material-symbols-rounded"
+          [class.rated]="rating >= 5"
+          aria-hidden="true"
+        >
+          star
+        </mat-icon>
+      </button>
     </div>
   `,
 })
