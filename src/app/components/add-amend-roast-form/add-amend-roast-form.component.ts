70421b7d8abe4b0d928c3fcfea46c4ed24b62a9f
Jordan Cooper <jordancooper1195@gmail.com>
2024-08-04 11:00:41 +1200

    Fixes rating for new roasts

--- src/app/components/add-amend-roast-form/add-amend-roast-form.component.ts
+++ src/app/components/add-amend-roast-form/add-amend-roast-form.component.ts
@@ -209,7 +209,9 @@ import { Roast } from '../../types/roast.type';
 
             <app-star-rating
               (valueChanged)="updateRating($event)"
-              [rating]="this.roastToUpdate.rating || 0"
+              [rating]="
+                roastToUpdate && roastToUpdate.rating ? roastToUpdate.rating : 0
+              "
             ></app-star-rating>
 
             <button
