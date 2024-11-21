175f58adffd80698907827c137f623752add7449
Jordan Cooper <jordancooper1195@gmail.com>
2024-11-22 08:45:10 +1300

    Adds form validation to registration page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
+  FormGroupDirective,
+  NgForm,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 
@@ -13,6 +18,30 @@ import { MatInputModule } from '@angular/material/input';
 
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+// This thing tells the AM-form-field to show / hide an error
+export class PasswordsEqualMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const confirmPassword = control!.value;
+    const password = form?.form.controls['password'].value;
+    const passwordsAreEqual = confirmPassword === password;
+    const passwordsAreNotEqual = !passwordsAreEqual && (control?.dirty || control?.touched);
+
+    return !!(passwordsAreNotEqual || form?.submitted);
+  }
+}
+
+// This thing specifies when error is passwordMissmatch
+const passwordsAreEqualValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const password = control!.value.password;
+  const confirmPassword = control!.value.confirmPassword;
+  const passwordsAreEqual = password === confirmPassword
+
+  return passwordsAreEqual ? null : { passwordMissmatch: true };
+};
 
 @Component({
   selector: 'app-register',
@@ -29,7 +58,7 @@ import { Router } from '@angular/router';
     <div>
       <h1>Register</h1>
 
-      <form [formGroup]="user">
+      <form [formGroup]="newUser">
         <mat-form-field appearance="outline">
           <mat-label>Email</mat-label>
           <input
@@ -40,8 +69,8 @@ import { Router } from '@angular/router';
             autocomplete="email"
             placeholder="you@example.com"
           />
-          @if (!invalidCredentialsSubmitted) {
-            <mat-error>Please check your email.</mat-error>
+          @if (email!.hasError('required')) {
+            <mat-error>Please enter your email address.</mat-error>
           }
         </mat-form-field>
 
@@ -55,41 +84,74 @@ import { Router } from '@angular/router';
             autocomplete="current-password"
           />
 
-          @if (invalidCredentialsSubmitted) {
-            <mat-error>Please check your email & password.</mat-error>
-          } @else {
-            <mat-error>Please check your password.</mat-error>
+          @if (password!.hasError('required')) {
+            <mat-error>Please enter a password</mat-error>
+          }
+        </mat-form-field>
+
+        <mat-form-field appearance="outline">
+          <mat-label>Confirm Password</mat-label>
+          <input
+            matInput
+            id="confirmPassword"
+            type="password"
+            formControlName="confirmPassword"
+            [errorStateMatcher]="passwordsEqualMatcher"
+          />
+
+          @if (confirmPassword!.hasError('required')) {
+            <mat-error>Please confirm your password.</mat-error>
+          } @else if (newUser.errors?.['passwordMissmatch']) {
+            <mat-error>Passwords don't match.</mat-error>
           }
         </mat-form-field>
 
         <button (click)="register()" mat-flat-button color="primary">
-          Log in
+          Sign up
         </button>
       </form>
     </div>
   `,
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private authService = inject(AuthService);
   private router = inject(Router);
 
-  invalidCredentialsSubmitted = false;
+  passwordsEqualMatcher = new PasswordsEqualMatcher();
 
-  user = new FormGroup({
-    email: new FormControl<string>('', {
-      validators: [Validators.required, Validators.email],
-      nonNullable: true,
-    }),
-    password: new FormControl<string>('', {
-      validators: [Validators.required],
-      nonNullable: true,
-    }),
-  });
+  newUser = new FormGroup(
+    {
+      email: new FormControl<string>('', {
+        validators: [Validators.required, Validators.email],
+        nonNullable: true,
+      }),
+      password: new FormControl<string>('', {
+        validators: [Validators.required],
+        nonNullable: true,
+      }),
+      confirmPassword: new FormControl<string>('', {
+        validators: [Validators.required],
+        nonNullable: true,
+      }),
+    }, { validators: passwordsAreEqualValidator }
+  );
+
+  public get email() {
+    return this.newUser.get('email');
+  }
+
+  public get password() {
+    return this.newUser.get('password');
+  }
+
+  public get confirmPassword() {
+    return this.newUser.get('confirmPassword');
+  }
 
   ngOnInit(): void {
-    if (this.authService.isLoggedIn) {
-      this.router.navigate(['/']);
-    }
+   if (this.authService.isLoggedIn) {
+     this.router.navigate(['/']);
+   }
   }
 
   register(): void {
