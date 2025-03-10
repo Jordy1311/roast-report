a1e883c7b4fe304d6444a430615bc61de2b0e68d
Jordan Cooper <jordancooper1195@gmail.com>
2025-03-11 12:01:11 +1300

    Fix router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet],
+  imports: [ RouterModule ],
   styleUrl: './app.component.scss',
   template: `<router-outlet></router-outlet>`
 })
