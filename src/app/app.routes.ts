fd524158a2a4f3d81c9bba5a33bf588f9e038dc0
Jordan Cooper <jordancooper1195@gmail.com>
2025-03-12 11:05:51 +1300

    Correct order of router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { isLoggedInGuard } from './guards/is-logged-in.guard';
+
 import { ConfirmLoginComponent } from './components/confirm-login/confirm-login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent,
-    canActivate: [isLoggedInGuard],
-    title: 'Roast Report',
-    pathMatch: 'full',
-  },
   {
     path: 'login',
     component: LoginComponent,
@@ -24,6 +18,13 @@ export const routes: Routes = [
     path: 'confirm-login/:confirmationCode',
     component: ConfirmLoginComponent,
     title: 'Roast Report | Confirm Login',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [isLoggedInGuard],
+    title: 'Roast Report',
     pathMatch: 'full',
   },
   {
